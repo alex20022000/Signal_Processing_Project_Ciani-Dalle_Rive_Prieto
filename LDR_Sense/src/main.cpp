9c3b8b724ab57a4383c679853540d91191175c43
alex20022000 <alexh.drp@gmail.com>
2024-10-13 15:12:47 -0300

    Cargo el proyecto actualizado de lectura de LDR

--- LDR_Sense/src/main.cpp
+++ LDR_Sense/src/main.cpp
@@ -4,10 +4,13 @@ hw_timer_t *timer = NULL;
 hw_timer_t *durationTimer = NULL;
 
 volatile bool lecturaFlag = false; // Flag para switchear en la callback
+volatile bool outputState = false; // Estado lógico de la salida
 //volatile bool buttonPressed = false; // Flag para indicar que se presionó el botón
 //volatile bool isReading = false; // Flag para indicar si estamos en periodo de lectura
 
 const int ldrPin = 35;
+const int outputPin = 23; // Pin de salida para testear frecuencia del timer
+
 //const int buttonPin = 23;
 
 // Función para calcular la frecuencia del timer a partir del prescaler
@@ -24,6 +27,8 @@ unsigned long frequencyToTicks(float frequencyHz, int prescaler) {
 // Callback del timer de lectura de datos
 void IRAM_ATTR lecturaLDR() {
   lecturaFlag = true;
+  outputState = !outputState; // Switchear el estado lógico de la salida
+  digitalWrite(outputPin, outputState); // Actualizar el pin de salida
 }
 /*
 // Callback del botón
@@ -41,6 +46,10 @@ void setup() {
   Serial.begin(115200);
   analogReadResolution(12); // Resolución del ADC
 
+  // Configurar el pin de salida
+  pinMode(outputPin, OUTPUT);
+  digitalWrite(outputPin, LOW); // Inicialmente en LOW
+
   //pinMode(buttonPin, INPUT_PULLUP); // Resistencia interna pull-up
   //attachInterrupt(digitalPinToInterrupt(buttonPin), onButtonPress, FALLING); // Interrupción de botón, callback y flanco descendente
   
@@ -49,7 +58,8 @@ void setup() {
   // Configuración del timer de lectura
   timer = timerBegin(0, prescaler, true); // Timer 0, prescaler 80, contador ascendente
   timerAttachInterrupt(timer, &lecturaLDR, true); // Vinculo la callback del timer
-  int desiredFrequencyHz = 200; // Frecuencia deseada en Hz
+  //int desiredFrequencyHz = 500; // Frecuencia deseada en Hz
+  int desiredFrequencyHz = 100; // Frecuencia deseada en Hz
   unsigned long ticks = frequencyToTicks(desiredFrequencyHz, prescaler);
   timerAlarmWrite(timer, ticks, true);
   timerAlarmEnable(timer);
